f1ae1c5d3f16f184ecbd4f2b8468ea14c21f122a
Geraldo Aparecido Borges <geraldo.estudos.trabalho@gmail.com>
2024-02-28 19:33:07 -0300

    add an option to delete an item

---
+++ src/app/components/thoughts/delete-thought/delete-thought.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Thought } from '../Thought';
+import { ThoughtService } from '../thought-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-delete-thought',
+  templateUrl: './delete-thought.component.html',
+  styleUrls: ['./delete-thought.component.css']
+})
+export class DeleteThoughtComponent implements OnInit {
+
+  currentThought: Thought = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: ThoughtService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.getById(parseInt(id!)).subscribe((thought) => {
+      this.currentThought = thought
+    })
+  }
+
+  deleteThought() {
+    if(this.currentThought.id) {
+      this.service.delete(this.currentThought.id).subscribe(() => {
+        this.router.navigate(['/listThoughts'])
+      })
+    }
+  }
+
+  cancel() {
+    this.router.navigate(['/listThoughts'])
+  }
+}
